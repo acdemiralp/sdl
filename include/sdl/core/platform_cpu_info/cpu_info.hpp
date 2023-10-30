dfcebebd3896259067f2effaab3b7a409c475da1
acdemiralp <demiralpali@gmail.com>
2023-10-30 04:52:23 +0100

    SDL_cpuinfo.h wrapper progress.

--- include/sdl/core/platform_cpu_info/cpu_info.hpp
+++ include/sdl/core/platform_cpu_info/cpu_info.hpp
@@ -3,157 +3,218 @@
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <span>
 #include <utility>
 
 #include <SDL2/SDL_cpuinfo.h>
 
 namespace sdl
 {
-inline std::int32_t   get_cpu_count          ()
+inline std::int32_t get_cpu_count          ()
 {
   return SDL_GetCPUCount();
 }
-inline std::int32_t   get_cpu_cache_line_size()
+inline std::int32_t get_cpu_cache_line_size()
 {
   return SDL_GetCPUCacheLineSize();
 }
 
 // Note: In mebibytes (1 mebibyte = 1.048576 megabytes).
-inline std::int32_t   get_system_ram         ()
+inline std::int32_t get_system_ram         ()
 {
   return SDL_GetSystemRAM();
 }
 
-inline bool           has_rdtsc              ()
+inline bool         has_rdtsc              ()
 {
   return SDL_HasRDTSC();
 }
-inline bool           has_altivec            ()
+inline bool         has_altivec            ()
 {
   return SDL_HasAltiVec();
 }
-inline bool           has_mmx                ()
+inline bool         has_mmx                ()
 {
   return SDL_HasMMX();
 }
-inline bool           has_3dnow              ()
+inline bool         has_3dnow              ()
 {
   return SDL_Has3DNow();
 }
-inline bool           has_sse                ()
+inline bool         has_sse                ()
 {
   return SDL_HasSSE();
 }
-inline bool           has_sse2               ()
+inline bool         has_sse2               ()
 {
   return SDL_HasSSE2();
 }
-inline bool           has_sse3               ()
+inline bool         has_sse3               ()
 {
   return SDL_HasSSE3();
 }
-inline bool           has_sse41              ()
+inline bool         has_sse41              ()
 {
   return SDL_HasSSE41();
 }
-inline bool           has_sse42              ()
+inline bool         has_sse42              ()
 {
   return SDL_HasSSE42();
 }
-inline bool           has_avx                ()
+inline bool         has_avx                ()
 {
   return SDL_HasAVX();
 }
-inline bool           has_avx2               ()
+inline bool         has_avx2               ()
 {
   return SDL_HasAVX2();
 }
-inline bool           has_avx512f            ()
+inline bool         has_avx512f            ()
 {
   return SDL_HasAVX512F();
 }
-inline bool           has_arm_simd           ()
+inline bool         has_arm_simd           ()
 {
   return SDL_HasARMSIMD();
 }
-inline bool           has_neon               ()
+inline bool         has_neon               ()
 {
   return SDL_HasNEON();
 }
-inline bool           has_lsx                ()
+inline bool         has_lsx                ()
 {
   return SDL_HasLSX();
 }
-inline bool           has_lasx               ()
+inline bool         has_lasx               ()
 {
   return SDL_HasLASX();
 }
 
-inline std::size_t    get_simd_alignment     ()
+inline std::size_t  get_simd_alignment     ()
 {
   return SDL_SIMDGetAlignment();
 }
-inline void*          simd_alloc             (              const std::size_t size)
+// Bad practice: You should use simd_make_unique<type>, simd_make_unique_array<type>, simd_make_shared<type> or simd_make_shared_array<type> instead.
+inline void*        simd_alloc             (              const std::size_t size)
 {
   return SDL_SIMDAlloc(size);
 }
-inline void*          simd_realloc           (void* memory, const std::size_t size)
+inline void*        simd_realloc           (void* memory, const std::size_t size)
 {
   return SDL_SIMDRealloc(memory, size);
 }
-inline void           simd_free              (void* memory)
+inline void         simd_free              (void* memory)
 {
   return SDL_SIMDFree(memory);
 }
 
-// Convenience.
+// Conveniences.
 
 template <typename type, typename... argument_types>
-type*                                 simd_new        (argument_types&&... arguments)
+type*                                               simd_new              (argument_types&&... arguments)
 {
   auto object = static_cast<type*>(simd_alloc(sizeof(type)));
-  new (object) type(std::forward<argument_types...>(arguments));
+  new (object) type(std::forward<argument_types>(arguments)...);
   return object;
 }
 template <typename type>
-void                                  simd_delete     (type*               object   )
+void                                                simd_delete           (type* object)
 {
   object->~type();
   simd_free(static_cast<void*>(object));
 }
 
+// Note: `std::span` is used instead of a raw pointer, as `simd_delete_array` requires the size.
 template <typename type, typename... argument_types>
-std::unique_ptr<type, void(*)(type*)> simd_make_unique (argument_types&&... arguments)
+std::span<type>                                     simd_new_array        (const std::size_t size, argument_types&&... arguments)
 {
-  return std::unique_ptr<type, void(*)(type*)>(simd_new<type>(std::forward<argument_types...>(arguments)), simd_delete<type>);
+  auto objects  = static_cast<type*>(simd_alloc(size * sizeof(type)));
+
+  auto iterator = objects;
+  for (std::size_t i = 0; i < size; ++i)
+    new (iterator++) type(std::forward<argument_types>(arguments)...);
+
+  return {objects, size};
 }
-template <typename type, typename... argument_types>
-std::shared_ptr<type>                 simd_make_shared (argument_types&&... arguments)
+// Note: `std::span` is used instead of a raw pointer, as `simd_delete_array` requires the size.
+template <typename type, typename initializer_type = type>
+std::span<type>                                     simd_new_array        (std::initializer_list<initializer_type> init)
 {
-  return std::shared_ptr<type>(simd_new<type>(std::forward<argument_types...>(arguments)), simd_delete<type>);
+  auto objects  = static_cast<type*>(simd_alloc(init.size() * sizeof(type)));
+
+  auto iterator = objects;
+  for (auto& object : init)
+    new (iterator++) type(object);
+
+  return {objects, init.size()};
 }
-
-/// TODO: Multi (array/vector/... container) new/delete, copy from existing, initializer list.
-
-template <typename type, typename... argument_types>
-type*                                 simd_array_new   (const std::size_t count, argument_types&&... arguments)
-{
-  auto array = static_cast<type*>(simd_alloc(count * sizeof(type)));
-
-  for (std::size_t i = 0; i < count; ++i)
-    new (array) type(std::forward<argument_types...>(arguments)); // TODO
-
-  return array;
-}
+// Note: `std::span` is used instead of a raw pointer, as `simd_delete_array` requires the size.
 template <typename type>
-void                                  simd_array_delete(type*             array)
+void                                                simd_delete_array     (const std::span<type>& objects)
 {
-  const auto count = sizeof(array) / sizeof(type);
+  auto iterator = objects.data();
+  for (std::size_t i = 0; i < objects.size(); ++i)
+    (iterator++)->~type();
 
-  for (std::size_t i = 0; i < count; ++i)
-    array->~type(); // TODO
-
-  simd_free(static_cast<void*>(array));
+  simd_free(static_cast<void*>(objects.data()));
 }
 
+template <typename type, typename... argument_types>
+std::unique_ptr<type, void(*)(type*)>               simd_make_unique      (argument_types&&... arguments)
+{
+  return std::unique_ptr<type, void(*)(type*)>(simd_new<type>(std::forward<argument_types>(arguments)...), simd_delete<type>);
+}
+template <typename type, typename... argument_types>
+std::shared_ptr<type>                               simd_make_shared      (argument_types&&... arguments)
+{
+  return std::shared_ptr<type>(simd_new<type>(std::forward<argument_types>(arguments)...), simd_delete<type>);
+}
+
+// Note: `std::function` is used instead of a function pointer in this case, as the deleter has to capture the size.
+template <typename type, typename... argument_types>
+std::unique_ptr<type[], std::function<void(type*)>> simd_make_unique_array(const std::size_t size, argument_types&&... arguments)
+{
+  return std::unique_ptr<type[], std::function<void(type*)>>(simd_new_array<type>(size, std::forward<argument_types>(arguments)...).data(),
+    [size] (type* objects)
+    {
+      simd_delete_array<type>(std::span<type>(objects, size));
+    });
+}
+// Note: `std::function` is used instead of a function pointer in this case, as the deleter has to capture the size.
+template <typename type, typename initializer_type = type>
+std::unique_ptr<type[], std::function<void(type*)>> simd_make_unique_array(std::initializer_list<initializer_type> init)
+{
+  return std::unique_ptr<type[], std::function<void(type*)>>(simd_new_array<type>(init).data(),
+    [size=init.size()](type* objects)
+    {
+      simd_delete_array<type>(std::span<type>(objects, size));
+    });
+}
+template <typename type, typename... argument_types>
+std::shared_ptr<type[]>                             simd_make_shared_array(const std::size_t size, argument_types&&... arguments)
+{
+  return std::shared_ptr<type[]>(simd_new_array<type>(size, std::forward<argument_types>(arguments)...).data(),
+    [size] (type* objects)
+    {
+      simd_delete_array<type>(std::span<type>(objects, size));
+    });
+}
+template <typename type, typename initializer_type = type>
+std::shared_ptr<type[]>                             simd_make_shared_array(std::initializer_list<initializer_type> init)
+{
+  return std::shared_ptr<type[]>(simd_new_array<type>(init).data(),
+    [size=init.size()] (type* objects)
+    {
+      simd_delete_array<type>(std::span<type>(objects, size));
+    });
+}
+
+/// TODO: SIMD std::allocator.
+template <typename type>
+struct simd_allocator
+{
+  
+};
+
+// Future direction: simd_renew and simd_renew_array (C++ realloc equivalents).
 }
--- tests/sdl_test.cpp
+++ tests/sdl_test.cpp
@@ -4,9 +4,12 @@
 
 #include <sdl/core/all.hpp>
 #include <sdl/core/basics/log.hpp>
+#include <sdl/core/platform_cpu_info/cpu_info.hpp>
 
 TEST_CASE("SDL Test")
 {
+  // Logging tests.
+
   const auto output_function = sdl::get_log_output_function();
 
   output_function(sdl::log_category::application, sdl::log_priority::info, "Testing output function getter.");
@@ -24,4 +27,68 @@ TEST_CASE("SDL Test")
   });
 
   sdl::log("Testing log output function setter 2.");
+
+  // Platform and cpu info tests.
+
+  std::cout << "get_cpu_count          : " << sdl::get_cpu_count          () << "\n";
+  std::cout << "get_cpu_cache_line_size: " << sdl::get_cpu_cache_line_size() << "B\n";
+
+  std::cout << "get_system_ram         : " << sdl::get_system_ram         () << "MiB\n";
+
+  std::cout << "has_rdtsc              : " << sdl::has_rdtsc              () << "\n";
+  std::cout << "has_altivec            : " << sdl::has_altivec            () << "\n";
+  std::cout << "has_mmx                : " << sdl::has_mmx                () << "\n";
+  std::cout << "has_3dnow              : " << sdl::has_3dnow              () << "\n";
+  std::cout << "has_sse                : " << sdl::has_sse                () << "\n";
+  std::cout << "has_sse2               : " << sdl::has_sse2               () << "\n";
+  std::cout << "has_sse3               : " << sdl::has_sse3               () << "\n";
+  std::cout << "has_sse41              : " << sdl::has_sse41              () << "\n";
+  std::cout << "has_sse42              : " << sdl::has_sse42              () << "\n";
+  std::cout << "has_avx                : " << sdl::has_avx                () << "\n";
+  std::cout << "has_avx2               : " << sdl::has_avx2               () << "\n";
+  std::cout << "has_avx512f            : " << sdl::has_avx512f            () << "\n";
+  std::cout << "has_arm_simd           : " << sdl::has_arm_simd           () << "\n";
+  std::cout << "has_neon               : " << sdl::has_neon               () << "\n";
+  std::cout << "has_lsx                : " << sdl::has_lsx                () << "\n";
+  std::cout << "has_lasx               : " << sdl::has_lasx               () << "\n";
+
+  std::cout << "get_simd_alignment     : " << sdl::get_simd_alignment     () << "B\n";
+
+  auto simd_ptr        = sdl::simd_new        <std::uint64_t>(1);
+  auto simd_unique_ptr = sdl::simd_make_unique<std::uint64_t>(2);
+  auto simd_shared_ptr = sdl::simd_make_shared<std::uint64_t>(3);
+  REQUIRE(*simd_ptr        == 1);
+  REQUIRE(*simd_unique_ptr == 2);
+  REQUIRE(*simd_shared_ptr == 3);
+  sdl::simd_delete(simd_ptr);
+  simd_unique_ptr.reset();
+  simd_shared_ptr.reset();
+
+  constexpr std::size_t array_size(8);
+
+  auto simd_array        = sdl::simd_new_array        <std::uint64_t>(array_size, 1);
+  auto simd_unique_array = sdl::simd_make_unique_array<std::uint64_t>(array_size, 2);
+  auto simd_shared_array = sdl::simd_make_shared_array<std::uint64_t>(array_size, 3);
+  for (std::size_t i = 0; i < array_size; ++i)
+  {
+    REQUIRE(simd_array             [i] == 1);
+    REQUIRE(simd_unique_array.get()[i] == 2);
+    REQUIRE(simd_shared_array.get()[i] == 3);
+  }
+  sdl::simd_delete_array(simd_array);
+  simd_unique_array.reset();
+  simd_shared_array.reset();
+
+  auto simd_array_init_list        = sdl::simd_new_array        <std::uint64_t>({0, 1, 2, 3, 4, 5, 6, 7});
+  auto simd_unique_array_init_list = sdl::simd_make_unique_array<std::uint64_t>({0, 1, 2, 3, 4, 5, 6, 7});
+  auto simd_shared_array_init_list = sdl::simd_make_shared_array<std::uint64_t>({0, 1, 2, 3, 4, 5, 6, 7});
+  for (std::size_t i = 0; i < array_size; ++i)
+  {
+    REQUIRE(simd_array_init_list             [i] == i);
+    REQUIRE(simd_unique_array_init_list.get()[i] == i);
+    REQUIRE(simd_shared_array_init_list.get()[i] == i);
+  }
+  sdl::simd_delete_array(simd_array_init_list);
+  simd_unique_array_init_list.reset();
+  simd_shared_array_init_list.reset();
 }
